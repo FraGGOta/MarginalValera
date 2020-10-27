9da35a1cf4a7a391d62a58db2f71dd1290fb41e7
JokePenny <S-total-war@mail.ru>
2020-10-27 18:39:08 +0700

    fix valera.rb

--- lib/valera.rb
+++ lib/valera.rb
@@ -1,13 +1,15 @@
 class Valera
-  attr_accessor :stats
+  attr_accessor :health
+  attr_accessor :mana
+  attr_accessor :positive
+  attr_accessor :tiredness
+  attr_accessor :money
 
   def initialize(health = 100, mana = 0, positive = 0, tiredness = 0, money = 0)
-    @stats = {
-      'health' => health,
-      'mana' => mana,
-      'positive' => positive,
-      'tiredness' => tiredness,
-      'money' => money
-    }
+    @health = health
+    @mana = mana
+    @positive = positive
+    @tiredness = tiredness
+    @money = money
   end
 end
