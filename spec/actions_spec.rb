251f4ef62e6ab1ac0a6149c07cb80d8705c41cda
fraggota <ramus99.99@mail.ru>
2020-11-22 17:07:11 +0700

    fixed test

--- spec/actions_spec.rb
+++ spec/actions_spec.rb
@@ -13,7 +13,15 @@ RSpec.describe Actions::ValeraActions do
     let(:settings) { config.fdata['default'] }
     let(:valera) { Valera.new(settings) }
 
-    it 'job' do
+    it 'start' do
+      expect(valera.health).to eq(100)
+      expect(valera.mana).to eq(0)
+      expect(valera.positive).to eq(0)
+      expect(valera.tiredness).to eq(0)
+      expect(valera.money).to eq(0)
+    end
+
+    it 'go_job' do
       ValeraJob.new(config.fdata['job'], valera).go_job
       expect(valera.health).to eq(100)
       expect(valera.mana).to eq(0)
@@ -22,7 +30,7 @@ RSpec.describe Actions::ValeraActions do
       expect(valera.money).to eq(100)
     end
 
-    it 'behold' do
+    it 'go_behold' do
       ValeraBehold.new(config.fdata['behold'], valera).go_behold
       expect(valera.health).to eq(100)
       expect(valera.mana).to eq(0)
@@ -31,7 +39,7 @@ RSpec.describe Actions::ValeraActions do
       expect(valera.money).to eq(0)
     end
 
-    it 'series' do
+    it 'go_series' do
       ValeraSeries.new(config.fdata['series'], valera).go_series
       expect(valera.health).to eq(95)
       expect(valera.mana).to eq(30)
@@ -40,7 +48,7 @@ RSpec.describe Actions::ValeraActions do
       expect(valera.money).to eq(-20)
     end
 
-    it 'bar' do
+    it 'go_bar' do
       ValeraBar.new(config.fdata['bar'], valera).go_bar
       expect(valera.health).to eq(90)
       expect(valera.mana).to eq(60)
@@ -49,7 +57,7 @@ RSpec.describe Actions::ValeraActions do
       expect(valera.money).to eq(-100)
     end
 
-    it 'drink' do
+    it 'go_drink' do
       ValeraDrink.new(config.fdata['drink'], valera).go_drink
       expect(valera.health).to eq(20)
       expect(valera.mana).to eq(90)
@@ -58,7 +66,7 @@ RSpec.describe Actions::ValeraActions do
       expect(valera.money).to eq(-150)
     end
 
-    it 'sing' do
+    it 'go_sing' do
       ValeraSing.new(config.fdata['sing'], valera).go_sing
       expect(valera.health).to eq(100)
       expect(valera.mana).to eq(10)
@@ -67,7 +75,7 @@ RSpec.describe Actions::ValeraActions do
       expect(valera.money).to eq(10)
     end
 
-    it 'sleep' do
+    it 'go_sleep' do
       ValeraSleep.new(config.fdata['sleep'], valera).go_sleep
       expect(valera.health).to eq(190)
       expect(valera.mana).to eq(0)
