10b7a36c3b6bf764eea86c07f8c509e553063030
fraggota <ramus99.99@mail.ru>
2020-11-22 16:53:58 +0700

    add test

--- spec/actions_spec.rb
+++ spec/actions_spec.rb
@@ -6,60 +6,74 @@ require './lib/actions/valera_sleep'
 require './lib/actions/valera_drink'
 require './lib/actions/valera_series'
 require './lib/actions/valera_action'
-require './lib/actions/valera_action'
-require './lib/actions/valera_action'
 
 RSpec.describe Actions::ValeraActions do
   describe 'ValeraActions' do
-	let(:config) {ConfigFile.new}
-	let(:settings) {config.fdata['default']}
-	let(:valera) {Valera.new(settings)}
-		 
+    let(:config) { ConfigFile.new }
+    let(:settings) { config.fdata['default'] }
+    let(:valera) { Valera.new(settings) }
+
     it 'job' do
-		ValeraJob.new(config.fdata['job'], valera).go_job
-        expect(valera.health).to eq (100)
-		expect(valera.mana).to eq (0)
-		expect(valera.positive).to eq (-10)
-		expect(valera.tiredness).to eq (70)
-		expect(valera.money).to eq (100)
+      ValeraJob.new(config.fdata['job'], valera).go_job
+      expect(valera.health).to eq(100)
+      expect(valera.mana).to eq(0)
+      expect(valera.positive).to eq(-10)
+      expect(valera.tiredness).to eq(70)
+      expect(valera.money).to eq(100)
     end
-	
-	it 'behold' do
-	  ValeraBehold.new(config.fdata['behold'], valera).go_behold
-        expect(valera.mana).to eq valera_expected.set_stats(100, 0, -10, 10, 0).mana
+
+    it 'behold' do
+      ValeraBehold.new(config.fdata['behold'], valera).go_behold
+      expect(valera.health).to eq(100)
+      expect(valera.mana).to eq(0)
+      expect(valera.positive).to eq(-10)
+      expect(valera.tiredness).to eq(10)
+      expect(valera.money).to eq(0)
     end
 
     it 'series' do
-	  ValeraSeries.new(config.fdata['series'], valera).go_series
-        expect(valera.positive).to eq valera_expected.set_stats(95, 30, -10, 10, -20).positive
+      ValeraSeries.new(config.fdata['series'], valera).go_series
+      expect(valera.health).to eq(95)
+      expect(valera.mana).to eq(30)
+      expect(valera.positive).to eq(-10)
+      expect(valera.tiredness).to eq(10)
+      expect(valera.money).to eq(-20)
     end
 
-    context 'bar' do
-      it {
-	  ValeraBar.new(config.fdata['bar'], valera).go_bar
-        expect(valera.tiredness).to eq  valera_expected.set_stats(90, 60, -10, 40, -100).tiredness
-      }
+    it 'bar' do
+      ValeraBar.new(config.fdata['bar'], valera).go_bar
+      expect(valera.health).to eq(90)
+      expect(valera.mana).to eq(60)
+      expect(valera.positive).to eq(-10)
+      expect(valera.tiredness).to eq(40)
+      expect(valera.money).to eq(-100)
     end
 
-    context 'drink' do
-      it {
-	  ValeraDrink.new(config.fdata['drink'], valera).go_drink
-        expect(valera.money).to eq valera_expected.set_stats(20, 90, -10, 80, -150).money
-      }
+    it 'drink' do
+      ValeraDrink.new(config.fdata['drink'], valera).go_drink
+      expect(valera.health).to eq(20)
+      expect(valera.mana).to eq(90)
+      expect(valera.positive).to eq(-10)
+      expect(valera.tiredness).to eq(80)
+      expect(valera.money).to eq(-150)
     end
 
-    context 'sing' do
-      it {
-	  ValeraSing.new(config.fdata['sing'], valera).go_sing
-        expect(valera.health).to eq valera_expected.set_stats(100, 10, -10, 20, 10).health
-      }
+    it 'sing' do
+      ValeraSing.new(config.fdata['sing'], valera).go_sing
+      expect(valera.health).to eq(100)
+      expect(valera.mana).to eq(10)
+      expect(valera.positive).to eq(-10)
+      expect(valera.tiredness).to eq(20)
+      expect(valera.money).to eq(10)
     end
 
-    context 'sleep' do
-      it {
-	  ValeraSleep.new(config.fdata['sleep'], valera).go_sleep
-        expect(valera.mana).to eq valera_expected.set_stats(190, 0, 0, 0, 0).mana
-      }
+    it 'sleep' do
+      ValeraSleep.new(config.fdata['sleep'], valera).go_sleep
+      expect(valera.health).to eq(190)
+      expect(valera.mana).to eq(0)
+      expect(valera.positive).to eq(0)
+      expect(valera.tiredness).to eq(0)
+      expect(valera.money).to eq(0)
     end
   end
 end
